76749908542bc7b1765ce73fe4fe617287cd4b00
Han <duhanx@163.com>
2021-10-08 21:59:44 +0800

    ...

--- src/views/dict/list.vue
+++ src/views/dict/list.vue
@@ -1,5 +1,12 @@
 <template>
     <div class="app-container">
+        <div class="el-toolbar">
+            <div class="el-toolbar-body" style="justify-content: flex-start;">
+                <el-button type="text" @click="exportData"><i class="fa fa-plus"/> 导出</el-button>
+                <el-button type="text"@click="importData"><i class="fa fa-plus"/> 导入</el-button>
+            </div>
+        </div>
+
         <el-table
         :data="list"
         style="width: 100%"
@@ -30,6 +37,28 @@
             </template>
             </el-table-column>
         </el-table>
+        <el-dialog title="导入":visible.sync="dialogImportVisible"width="480px">
+            <el-form label-position="right" label-width="170px">
+
+            <el-form-item label="文件">
+            <el-upload
+            :multiple="false"
+            :on-success="onUploadSuccess"
+            :action="'http://localhost:8202/admin/cmn/dict/importData'"
+            class="upload-demo">
+            <el-button size="small" type="primary">点击上传</el-button>
+            <div slot="tip" class="el-upload__tip">只能上传xls文件，且不超过500kb</div>
+            </el-upload>
+            </el-form-item>
+
+            </el-form>
+        <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogImportVisible = false">
+            取消
+        </el-button>
+        </div>
+        </el-dialog>
+
     </div>
 </template>
 <script>
@@ -37,6 +66,7 @@ import dict from '@/api/dict'
 export default{
     data() {
         return {
+            dialogImportVisible:false, //设置弹框是否弹出
             list:[]   //数据字典列表的数组
         }
     },
@@ -56,6 +86,21 @@ export default{
                 .then(response =>{
                     resolve(response.data)
                 })
+        },
+        //导出数据字典
+        exportData(){
+            window.location.href= 'http://localhost:8202/admin/cmn/dict/exportData'
+        },
+        //导入数据字典
+        importData(){
+            this.dialogImportVisible = true
+        },
+        //上传成功调用
+        onUploadSuccess(){
+            //关闭弹框
+            this.dialogImportVisible = false
+            //刷新页面
+            this.getDictList(1)
         }
     }
 
